63eb1a6f8e443fb8d449ae68aa85ed55e3de3dee
Michael Wellington <79681964+mickwelli@users.noreply.github.com>
2023-08-22 16:49:03 +0930

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -211,7 +211,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxpHJVgCM7KxLr7HNlYAEKU-Il2liOd1A9EfDYtnb6bzdKPyVhDT0gizc4Qqy9wuQ3a/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyyKjopXCuqT_wNkbmN0AF6-XFeX_xgoTpZlU_UrxzGo71-I6g6PYEXzGOjh7nikAn33w/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
